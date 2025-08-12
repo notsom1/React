20eaa3071f080c8c1ba326890a25057244648b57
notsom1 <mihawk51@gmail.com>
2025-08-12 16:15:08 +0300

    update

--- Section 5 Practice/src/App.jsx
+++ Section 5 Practice/src/App.jsx
@@ -1,7 +1,35 @@
+import { EntryFields } from "./components/EntryFields";
+import { Header } from "./components/Header";
+import { ResultsTable } from "./components/ResultsTable";
+import { calculateInvestmentResults } from "./util/investment";
+import { useState } from "react";
+
 function App() {
+  const [data, setData] = useState([]);
+
+  function calculateResult({
+    initialInvestment,
+    annualInvestment,
+    expectedReturn,
+    duration,
+  }) {
+    const asd = calculateInvestmentResults({
+      initialInvestment,
+      annualInvestment,
+      expectedReturn,
+      duration,
+    });
+    setData(asd);
+
+    console.log(asd);
+  }
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <>
+      <Header></Header>
+      <EntryFields calculate={calculateResult}></EntryFields>
+      <ResultsTable data={data}></ResultsTable>
+    </>
+  );
 }
 
-export default App
+export default App;
