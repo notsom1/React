2a83b8deb54dd67aa53bbea28e309c68746b083e
notsom1 <aaldulaie@gmail.com>
2025-08-13 12:10:10 +0300

    asd

--- Section 8 Refs and Portals/src/App.jsx
+++ Section 8 Refs and Portals/src/App.jsx
@@ -1,10 +1,19 @@
-import Player from './components/Player.jsx';
+import Player from "./components/Player.jsx";
+import TimerChallenge from "./components/TimerChallenge.jsx";
 
 function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimerChallenge title="Easy" targetTime={1}></TimerChallenge>
+        <TimerChallenge title="Not Easy" targetTime={5}></TimerChallenge>
+        <TimerChallenge title="Hard" targetTime={10}></TimerChallenge>
+        <TimerChallenge
+          title="Very Hard Actually"
+          targetTime={15}
+        ></TimerChallenge>
+      </div>
     </>
   );
 }
