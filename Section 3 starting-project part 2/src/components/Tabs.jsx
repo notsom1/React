fbc1046d9952c4baaf78f945f8ff490174e2332d
notsom1 <mihawk51@gmail.com>
2025-08-28 17:09:03 +0300

    merged

--- Section 3 starting-project part 2/src/components/Tabs.jsx
+++
@@ -1,8 +0,0 @@
-export default function Tabs({ buttons, children, ButtonContainer }) {
-  return (
-    <>
-      <ButtonContainer>{buttons}</ButtonContainer>
-      {children}
-    </>
-  );
-}
--- Section 5 Practice/src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import logo from "../assets/investment-calculator-logo.png";
-export function Header() {
-  return (
-    <header id="header">
-      <img src={logo}></img>
-      <h1>Investment Calculator</h1>
-    </header>
-  );
-}
