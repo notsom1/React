6cebe3aac3e74ea1bb66292befcda4b037ac8654
notsom1 <mihawk51@gmail.com>
2025-08-28 16:20:33 +0300

    asd

--- Section-10-Context-API-and-useReducer/src/components/Cart.jsx
+++ Section-10-Context-API-and-useReducer/src/components/Cart.jsx
@@ -1,5 +1,9 @@
-export default function Cart({ items, onUpdateItemQuantity }) {
-  const totalPrice = items.reduce(
+import { useContext } from "react";
+import { CartContext } from "../store/shopping-cart-context";
+
+export default function Cart() {
+  const cartContext = useContext(CartContext);
+  const totalPrice = cartContext.items.reduce(
     (acc, item) => acc + item.price * item.quantity,
     0
   );
@@ -7,10 +11,10 @@ export default function Cart({ items, onUpdateItemQuantity }) {
 
   return (
     <div id="cart">
-      {items.length === 0 && <p>No items in cart!</p>}
-      {items.length > 0 && (
+      {cartContext.items.length === 0 && <p>No items in cart!</p>}
+      {cartContext.items.length > 0 && (
         <ul id="cart-items">
-          {items.map((item) => {
+          {cartContext.items.map((item) => {
             const formattedPrice = `$${item.price.toFixed(2)}`;
 
             return (
@@ -20,11 +24,19 @@ export default function Cart({ items, onUpdateItemQuantity }) {
                   <span> ({formattedPrice})</span>
                 </div>
                 <div className="cart-item-actions">
-                  <button onClick={() => onUpdateItemQuantity(item.id, -1)}>
+                  <button
+                    onClick={() =>
+                      cartContext.updateCartItemQuantity(item.id, -1)
+                    }
+                  >
                     -
                   </button>
                   <span>{item.quantity}</span>
-                  <button onClick={() => onUpdateItemQuantity(item.id, 1)}>
+                  <button
+                    onClick={() =>
+                      cartContext.updateCartItemQuantity(item.id, 1)
+                    }
+                  >
                     +
                   </button>
                 </div>
